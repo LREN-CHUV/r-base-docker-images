0b02d4b935c9f167421d63899c378e32c45262df
Ludovic Claude <ludovic.claude54@gmail.com>
2015-07-03 11:48:40 +0200

    Add MASS library to R

--- hbp/r-jdbc/build.sh
+++ hbp/r-jdbc/build.sh
@@ -8,6 +8,10 @@ for pkg in DBI_0.3.1.tar.gz rJava_0.9-6.tar.gz RJDBC_0.2-5.tar.gz ; do
 	[ -f $pkg ] || curl -O http://cran.r-project.org/src/contrib/$pkg
 done
 
+for pkg in postgresql-9.3-1103.jdbc41.jar postgresql-9.4-1201.jdbc41.jar ; do
+	[ -f $pkg ] || curl -O https://jdbc.postgresql.org/download/$pkg
+done
+
 cd ..
 
 sudo docker build -t hbp/r-jdbc .
