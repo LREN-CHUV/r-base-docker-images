784000e79700f99c6ca8059f7a5449a772d9080b
Ludovic Claude <ludovic.claude54@gmail.com>
2015-07-06 12:27:52 +0200

    Fix typos

--- hbp-node/r-linear-regression/run.sh
+++ hbp-node/r-linear-regression/run.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-http -v DELETE localhost:4400/scheduler/job/r-linerar-regression-002
+request_id=$(grep -E "request_id=.*" -o run.ini | cut -d'=' -f2)
+http -v DELETE localhost:4400/scheduler/job/r-linear-regression-$request_id
 
 j2 run.json.j2 run.ini > run.json
 http -v --json POST localhost:4400/scheduler/iso8601 < run.json
