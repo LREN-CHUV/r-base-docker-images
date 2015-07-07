ec27dade6efc4cfba893c8d267a1d4c5218bbb21
Ludovic Claude <ludovic.claude54@gmail.com>
2015-07-07 12:49:27 +0200

    Run computations on the hospital nodes

--- hbp-federation/workflow/downloads/workflow.sh
+++ hbp-federation/workflow/downloads/workflow.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-/opt/call-nodes/run.sh 192.168.33.10:4400 /opt/call-nodes/run.ini &
-/opt/call-nodes/run.sh 192.168.33.11:4400 /opt/call-nodes/run2.ini &
+cd /opt/call-nodes/
+./run.sh 192.168.33.10:4400 run.ini &
+./run.sh 192.168.33.11:4400 run2.ini &
+
 java -jar /opt/workflow/workflow.jar
