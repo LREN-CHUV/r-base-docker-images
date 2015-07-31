92138d143f5b98899c1d560bdc3546385a2cb996
Ludovic Claude <ludovic.claude54@gmail.com>
2015-07-31 17:28:38 +0200

    Improve setup

--- setup.sh
+++ setup.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
 
-# Install Docker (https://docs.docker.com/installation/ubuntulinux/)
-# then run this script to complete the installation of tools used in this project.
-
-sudo /bin/sh -c "curl -L https://github.com/docker/compose/releases/download/1.3.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
-sudo chmod a+x /usr/local/bin/docker-compose
-
-sudo /bin/sh -c "curl -L https://github.com/harbur/captain/releases/download/v0.4.0/captain > /usr/local/bin/captain"
-sudo chmod a+x /usr/local/bin/captain
+cd ../dev-setup
+./setup.sh --tags containers_build
