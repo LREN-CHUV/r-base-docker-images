f08b45e4a254d813b96a0a81ef3e56b1853a0069
Ludovic Claude <ludovic.claude54@gmail.com>
2015-08-07 19:45:01 +0200

    Update workflow

--- hbp_federation/workflow/build.sh
+++ hbp_federation/workflow/build.sh
@@ -1,21 +1,2 @@
-#!/bin/bash
-
-get_script_dir () {
-     SOURCE="${BASH_SOURCE[0]}"
-     while [ -h "$SOURCE" ]; do
-          DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-          SOURCE="$( readlink "$SOURCE" )"
-          [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
-     done
-     $( cd -P "$( dirname "$SOURCE" )" )
-     pwd
-}
-
-ROOT_DIR="$(get_script_dir)"/../..
-
-mkdir -p downloads/call-nodes/
-
-#cp $ROOT_DIR/hbp-node/r-linear-regression/run* downloads/call-nodes/
-cp $ROOT_DIR/../workflow/target/workflow-1.0-SNAPSHOT.jar downloads/workflow.jar
-
-sudo docker build -t hbp_federation/workflow .
+#!/bin/sh -e
+captain build
--- hbp_federation/workflow/test.sh
+++ hbp_federation/workflow/test.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 docker run -i -t --rm \
   -e REQUEST_ID=001 \
-  -e COUNT_RESULTS_QUERY="select count(*) from results_linear_regression where request_id='002'" \
+  -e COUNT_RESULTS_QUERY="select count(*) from results_linear_regression where request_id='001'" \
   -e EXPECTED_COUNT_RESULTS=2 \
   -e JDBC_DRIVER=org.postgresql.Driver \
   -e JDBC_JAR_PATH=/usr/lib/R/libraries/postgresql-9.3-1103.jdbc41.jar \
-  -e JDBC_URL=jdbc:postgresql://155.105.158.141:5432/CHUV_MIPS \
-  -e JDBC_USER=postgres \
-  -e JDBC_PASSWORD=Mordor1975 \
+  -e JDBC_URL=jdbc:postgresql://lab01560.intranet.chuv:5432/DEMO \
+  -e JDBC_USER=federation \
+  -e JDBC_PASSWORD=HBP4thewin \
   -v /home/ludovic/tmp:/log \
-  hbp_federation/workflow /bin/bash
+  registry.federation.mip.hbp/hbp_federation/workflow /bin/bash
