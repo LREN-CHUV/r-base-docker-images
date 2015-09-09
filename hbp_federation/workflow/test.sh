6f4d1c9c004736c5b962b906a0a6f63df98a2918
Ludovic Claude <ludovic.claude54@gmail.com>
2015-09-09 15:16:51 +0200

    Reuse existing image with Denodo driver

--- hbp_federation/workflow/test.sh
+++ hbp_federation/workflow/test.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 docker run -i -t --rm \
-  -e REQUEST_ID=001 \
+  -e REQUEST_ID=002 \
   -e COUNT_RESULTS_QUERY="select count(*) from results_linear_regression where request_id='001'" \
   -e EXPECTED_COUNT_RESULTS=2 \
-  -e JDBC_DRIVER=org.postgresql.Driver \
-  -e JDBC_JAR_PATH=/usr/lib/R/libraries/postgresql-9.3-1103.jdbc41.jar \
-  -e JDBC_URL=jdbc:postgresql://lab01560.intranet.chuv:5432/DEMO \
-  -e JDBC_USER=federation \
-  -e JDBC_PASSWORD=HBP4thewin \
-  -v /home/ludovic/tmp:/log \
-  registry.federation.mip.hbp/hbp_federation/workflow /bin/bash
+  -e JDBC_DRIVER=com.denodo.vdp.jdbc.Driver \
+  -e JDBC_JAR_PATH=/usr/lib/R/libraries/denodo-vdp-jdbcdriver.jar \
+  -e JDBC_URL=jdbc:vdb://lab01560.intranet.chuv:9999/analytics \
+  -e JDBC_USER=analytics \
+  -e JDBC_PASSWORD="HBP\=neuroinfo" \
+  registry.federation.mip.hbp/hbp_federation/workflow
