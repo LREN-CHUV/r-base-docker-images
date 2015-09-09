50d4e329ba2e84ee6e62d768df48448b6eaf234f
Ludovic Claude <ludovic.claude54@gmail.com>
2015-09-09 20:20:42 +0200

    Simplify job submission to local nodes
    Former-commit-id: 240bc22c8d8091f1163627fdb9ab45eea6d59ca1

--- hbp_federation/workflow/downloads/call-nodes/run.sh
+++ hbp_federation/workflow/downloads/call-nodes/run.sh
@@ -8,9 +8,25 @@ cd /opt/call-nodes/
 NODE1=155.105.202.126:14400
 NODE2=155.105.202.126:24400
 
-request_id=$(grep -E "request_id=.*" -o run.ini | cut -d'=' -f2)
-http -v DELETE $NODE1/scheduler/job/r-linear-regression-$request_id-CHUV
-http -v DELETE $NODE2/scheduler/job/r-linear-regression-$request_id-Brescia
+http -v DELETE $NODE1/scheduler/job/r-linear-regression-$REQUEST_ID-CHUV
+http -v DELETE $NODE2/scheduler/job/r-linear-regression-$REQUEST_ID-Brescia
 
-(j2 run.json.j2 run.ini > run.json && http_proxy="" http -v --json POST $NODE1/scheduler/iso8601 < run.json && rm run.json) &
-(j2 run.json.j2 run2.ini > run2.json && http_proxy="" http -v --json POST $NODE2/scheduler/iso8601 < run2.json && rm run2.json) &
+(
+	NODE=CHUV \
+      IN_JDBC_URL=jdbc:postgresql://hbps2.intranet.chuv:5432/LDSM1 \
+      OUT_JDBC_URL=jdbc:postgresql://hbps2.intranet.chuv:31432/analytics \
+      j2 -f env run.ini.j2 > run1.ini
+    j2 run.json.j2 run1.ini > run1.json
+    http_proxy="" http -v --json POST $NODE1/scheduler/iso8601 < run1.json
+    rm run1.json
+) &
+
+(
+	NODE=CHUV-ADNI \
+      IN_JDBC_URL=jdbc:postgresql://hbps3.intranet.chuv:5432/LDSM3 \
+      OUT_JDBC_URL=jdbc:postgresql://hbps3.intranet.chuv:31432/analytics \
+      j2 -f env run.ini.j2 > run2.ini
+    j2 run.json.j2 run2.ini > run2.json
+    http_proxy="" http -v --json POST $NODE1/scheduler/iso8601 < run2.json
+    rm run2.json
+) &
