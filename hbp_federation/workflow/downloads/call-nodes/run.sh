69d3e1e93df2e01607c7a71f4a37bc49fd2b066a
Ludovic Claude <ludovic.claude54@gmail.com>
2015-09-10 19:22:08 +0200

    Don't use httppie anymore
    Former-commit-id: 304ab04bc1288d981778280fa03984eee3d120cf

--- hbp_federation/workflow/downloads/call-nodes/run.sh
+++ hbp_federation/workflow/downloads/call-nodes/run.sh
@@ -6,14 +6,14 @@ cd /opt/call-nodes/
 #NODE2=hbps3.chuv.ch:4400
 
 (
-	export URL=155.105.202.126:14400
-	export NODE=CHUV
+    export URL=155.105.202.126:14400
+    export NODE=CHUV
     export IN_JDBC_URL=jdbc:postgresql://hbps2.intranet.chuv:5432/LDSM1
     export OUT_JDBC_URL=jdbc:postgresql://hbps2.intranet.chuv:31432/analytics
 
-	curl -v \
-	  -XDELETE \
-	  $URL/scheduler/job/r-linear-regression-$REQUEST_ID-$NODE
+    curl -v \
+      -XDELETE \
+      $URL/scheduler/job/r-linear-regression-$REQUEST_ID-$NODE
 
     j2 -f env run.ini.j2 > run1.ini
     j2 run.json.j2 run1.ini > run1.json
@@ -26,14 +26,14 @@ cd /opt/call-nodes/
 )
 
 (
-	export URL=155.105.202.126:24400
-	export NODE=CHUV-ADNI
+    export URL=155.105.202.126:24400
+    export NODE=CHUV-ADNI
     export IN_JDBC_URL=jdbc:postgresql://hbps3.intranet.chuv:5432/LDSM3
     export OUT_JDBC_URL=jdbc:postgresql://hbps3.intranet.chuv:31432/analytics
 
-	curl -v \
-	  -XDELETE \
-	  $URL/scheduler/job/r-linear-regression-$REQUEST_ID-$NODE
+    curl -v \
+      -XDELETE \
+      $URL/scheduler/job/r-linear-regression-$REQUEST_ID-$NODE
     j2 -f env run.ini.j2 > run2.ini
     j2 run.json.j2 run2.ini > run2.json
     curl -v \
--- hbp_federation/workflow/run.sh
+++ hbp_federation/workflow/run.sh
@@ -16,8 +16,14 @@ LR_DIR=$DIR/../r-linear-regression
 CHRONOS_FEDERATION=lab01560.intranet.chuv:4400
 
 request_id=$(grep -E "^request_id=.*" -o run.ini | cut -d'=' -f2)
-http_proxy="" http -v DELETE $CHRONOS_FEDERATION/scheduler/job/federation-workflow-$request_id
-http_proxy="" http -v DELETE $CHRONOS_FEDERATION/scheduler/job/r-federation-linear-regression-$request_id
+http_proxy="" \
+  curl -v \
+    -XDELETE \
+    $CHRONOS_FEDERATION/scheduler/job/federation-workflow-$request_id
+http_proxy="" \
+  curl -v \
+    -XDELETE \
+    $CHRONOS_FEDERATION/scheduler/job/r-federation-linear-regression-$request_id
 
 lsof -i :14400 > /dev/null || ssh -f ludovic@hbps2.intranet.chuv -L 0.0.0.0:14400:hbps2.intranet.chuv:4400 -N
 lsof -i :24400 > /dev/null || ssh -f ludovic@hbps3.intranet.chuv -L 0.0.0.0:24400:hbps3.intranet.chuv:4400 -N
