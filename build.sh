cc880e33602f282283f2770b095f2994dfd0cc0d
Ludovic Claude <ludovic.claude54@gmail.com>
2015-11-05 16:42:35 +0100

    New layer r-libs
    Former-commit-id: 28a142d7d055e529cc75c7180d0d53ac15905c00

--- build.sh
+++ build.sh
@@ -15,9 +15,9 @@ get_script_dir () {
 ROOT_DIR="$(get_script_dir)"
 
 for image in mip_base/r-base \
+             mip_base/r-libs \
              mip_base/r-java \
              mip_base/r-database \
-             mip_base/r-database-mass \
              mip_tools/r-interactive \
              mip_federation/workflow; do
 
