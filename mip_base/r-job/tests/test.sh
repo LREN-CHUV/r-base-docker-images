e025e86afbe3a02598d36c4d13821c4f9da0e7fd
Ludovic Claude <ludovic.claude@laposte.net>
2015-11-08 23:52:56 +0100

    Fix test script
    Former-commit-id: d591a1cb5d557fed3717081d6e968cc19aa3f262

--- mip_base/r-job/tests/test.sh
+++ mip_base/r-job/tests/test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-docker run -i -t --rm \
+docker run --rm \
   --link indb:indb \
   --link analyticsdb:outdb \
   -e JOB_ID=001 \
@@ -15,4 +15,4 @@ docker run -i -t --rm \
   -e OUT_JDBC_URL=jdbc:postgresql://outdb:5432/postgres \
   -e OUT_JDBC_USER=postgres \
   -e OUT_JDBC_PASSWORD=test \
-  registry.federation.mip.hbp/mip_base/r-job-test shell
+  registry.federation.mip.hbp/mip_base/r-job-test test
