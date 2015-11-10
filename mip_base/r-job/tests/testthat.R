0683cdac1077babe7e8ee4f0ba9873a6ec48e876
Ludovic Claude <ludovic.claude54@gmail.com>
2015-11-10 18:06:50 +0100

    All tests pass
    Former-commit-id: 6359831970c1c3024d7690d11aa0e8cdbd7ef089

--- mip_base/r-job/tests/testthat.R
+++ mip_base/r-job/tests/testthat.R
@@ -1,19 +1,29 @@
-library(testthat)
-
-library(hbpjdbcconnect)
+library(testthat);
+library(hbpjdbcconnect);
 
 connect2indb();
 
-print ("connected to in db")
+print ("[ok] Connected to in db");
 
-df <- fetchData("select id, a, b from some_data")
+df <- fetchData("select id, a, b from some_data");
 
-print (df)
+expect_equal(nrow(df), 2);
+
+Sys.sleep(1);
 
 connect2outdb();
 
-print ("connected to out db")
+print ("[ok] Connected to out db");
 
-saveResults(df)
+saveResults(df);
 
-print ("TODO")
+library(RJDBC);
+connect2outdb();
+res <- RJDBC::dbGetQuery(out_conn, "select * from job_result");
+disconnectdbs();
+
+expect_equal(res[1,'job_id'], '001');
+expect_equal(res[1,'node'],   'job_test');
+expect_equal(res[1,'data'],   '[{"id":"001","a":1,"b":2},{"id":"002","a":2,"b":3}]');
+
+print ("[ok] Test passed");
