227365db39c638030ebb7782e33bb8b59ac23fa3
Ludovic Claude <ludovic.claude54@gmail.com>
2015-12-09 17:28:00 +0100

    Fixing r-job
    Former-commit-id: 191e8bf11defea57b58796651eeb7f251b59e674

--- mip_base/r-job/tests/test.sh
+++ mip_base/r-job/tests/test.sh
@@ -1,5 +1,23 @@
 #!/bin/sh
 
+get_script_dir () {
+     SOURCE="${BASH_SOURCE[0]}"
+
+     while [ -h "$SOURCE" ]; do
+          DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+          SOURCE="$( readlink "$SOURCE" )"
+          [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+     done
+     cd -P "$( dirname "$SOURCE" )"
+     pwd
+}
+
+DIR=$(get_script_dir)
+ROOT=$DIR/../../..
+
+$ROOT/tests/in-db/start-db.sh
+$DIR/out-db/start-db.sh
+
 docker run --rm \
   --link indb:indb \
   --link analyticsdb:outdb \
@@ -16,3 +34,6 @@ docker run --rm \
   -e OUT_JDBC_USER=postgres \
   -e OUT_JDBC_PASSWORD=test \
   registry.federation.mip.hbp/mip_base/r-job-test test
+
+$ROOT/tests/in-db/stop-db.sh
+$DIR/out-db/stop-db.sh
