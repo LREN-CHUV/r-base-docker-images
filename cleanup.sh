3f0122f558387b33a79b5e8a62d7204e81d5ed2b
Ludovic Claude <ludovic.claude54@gmail.com>
2016-01-18 12:09:48 +0100

    Improve cleanup of Docker containers
    Former-commit-id: 9df93a9f4b529d7ffcc2cdc1aed2d47a34dec613

--- cleanup.sh
+++ cleanup.sh
@@ -11,17 +11,17 @@ else
 fi
 
 if [[ "$tag" == "" ]]; then
-	echo "Usage: ./cleanup.sh <tag>"
+    echo "Usage: ./cleanup.sh <tag>"
     echo "where tag can be one of:"
     $DOCKER images | grep "registry.federation.mip.hbp/" | awk '{print $2}' | sort | uniq
-	exit 1
+    exit 1
 fi
 
 # Remove exited containers
-$DOCKER rm $($DOCKER ps -q -f status=exited)
+$DOCKER ps -q -f status=exited | xargs --no-run-if-empty $DOCKER rm
 
 # Remove tagged images
-$DOCKER images | grep "registry.federation.mip.hbp/" | grep $tag | cut -d' ' -f1 | sed -e "s/$/:$tag/" | xargs docker rmi
+$DOCKER images | grep "registry.federation.mip.hbp/" | grep $tag | cut -d' ' -f1 | sed -e "s/$/:$tag/" | xargs --no-run-if-empty docker rmi
 
 # Remove anomymous images
-$DOCKER rmi $($DOCKER images -f "dangling=true" -q)
+$DOCKER images -f "dangling=true" -q | xargs --no-run-if-empty $DOCKER rmi
