dd9e28e876e71a486c6f36f3f018964f9e381cb9
Ludovic Claude <ludovic.claude54@gmail.com>
2016-03-10 17:10:53 +0100

    Update gosu

--- r-images/r-mip/scripts/setup.sh
+++ r-images/r-mip/scripts/setup.sh
@@ -38,10 +38,10 @@ rm -rf /var/lib/apt/lists/*
 apt-get update
 
 # Grab gosu for easy step-down from root
-
+GOSU_VERSION=1.7
 gpg --keyserver pool.sks-keyservers.net --recv-keys B42F6819007F00F88E364FD4036A9C25BF357DD4
-wget -O /usr/local/bin/gosu "https://github.com/tianon/gosu/releases/download/1.2/gosu-$(dpkg --print-architecture)"
-wget -O /usr/local/bin/gosu.asc "https://github.com/tianon/gosu/releases/download/1.2/gosu-$(dpkg --print-architecture).asc"
+wget -O /usr/local/bin/gosu "https://github.com/tianon/gosu/releases/download/$GOSU_VERSION/gosu-$(dpkg --print-architecture)"
+wget -O /usr/local/bin/gosu.asc "https://github.com/tianon/gosu/releases/download/$GOSU_VERSION/gosu-$(dpkg --print-architecture).asc"
 gpg --verify /usr/local/bin/gosu.asc
 rm /usr/local/bin/gosu.asc
 chmod +x /usr/local/bin/gosu
