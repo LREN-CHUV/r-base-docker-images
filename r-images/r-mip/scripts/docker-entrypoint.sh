f0c2027bd33783b9d23ea532dad634269292ff83
Ludovic Claude <ludovic.claude54@gmail.com>
2016-03-16 15:14:46 +0100

    Use exec in docker-entrypoint.sh
    See https://docs.docker.com/engine/userguide/eng-image/dockerfile_best-practices/#entrypoint

--- r-images/r-mip/scripts/docker-entrypoint.sh
+++ r-images/r-mip/scripts/docker-entrypoint.sh
@@ -1,36 +1,40 @@
-#!/bin/sh -e
+#!/bin/bash
+set -e
 
 if [ "$1" = "compute" ]; then
 	mkdir -p "$COMPUTE_IN" "$COMPUTE_OUT"
 	chown -R compute "$COMPUTE_IN" "$COMPUTE_OUT"
 
-	gosu compute /usr/bin/Rscript /src/main.R
-	exit $?
+	exec gosu compute /usr/bin/Rscript /src/main.R
 
 elif [ "$1" = "test" ]; then
 	mkdir -p "$COMPUTE_IN" "$COMPUTE_OUT"
 	chown -R compute "$COMPUTE_IN" "$COMPUTE_OUT"
 
-	gosu compute /usr/bin/Rscript /src/tests/testthat.R
-	exit $?
+	exec gosu compute /usr/bin/Rscript /src/tests/testthat.R
 
 elif [ "$1" = "export" ]; then
 	# Assume that $COMPUTE_OUT is mounted on the host directory
 	cp -R /src/* "$COMPUTE_OUT/"
+
 elif [ "$1" = "export-docs" ]; then
 	# Assume that $COMPUTE_OUT is mounted on the host directory
 	cp -R /var/www/html/* "$COMPUTE_OUT/"
+
 elif [ "$1" = "serve" ]; then
 	/usr/sbin/nginx
+
 elif [ "$1" = "/bin/sh" ] | [ "$1" = "/bin/bash" ] ; then
 	echo "** Please use 'shell' command instead of $1 **"
-	/bin/bash -i
+	exec /bin/bash -i
+
 elif [ "$1" = "shell" ] ; then
-	/bin/bash -i
+	exec /bin/bash -i
+
 elif [ "$1" = "R" ] ; then
 	cd /src
-	gosu compute /usr/bin/R
-	exit $?
+	exec gosu compute /usr/bin/R
+
 elif [ "$1" = "help" ] | [ "$1" = "-h" ] | [ "$1" = "--help" ] | [ "$1" = "" ] ; then
 	echo "Usage:"
 	echo
