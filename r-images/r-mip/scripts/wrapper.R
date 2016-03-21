6cd5c176dc96b8ff2c0aca24483ffbd875d1e0d7
Ludovic Claude <ludovic.claude54@gmail.com>
2016-03-21 12:44:27 +0100

    Suppress messages

--- r-images/r-mip/scripts/wrapper.R
+++ r-images/r-mip/scripts/wrapper.R
@@ -1,4 +1,4 @@
-library(hbpjdbcconnect);
+suppressMessages(library(hbpjdbcconnect));
 
 errFile <- file(Sys.getenv("ERROR_FILE", "/data/out/errors.txt"), open="wt");
 outFile <- file(Sys.getenv("OUTPUT_FILE", "/data/out/output.txt"), open="wt");
