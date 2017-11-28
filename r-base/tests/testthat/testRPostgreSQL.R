4e3588fb710da9ed20b3f9a1a12725ae78aff907
Ludovic Claude <ludovic.claude54@gmail.com>
2017-11-28 16:54:10 +0100

    Format code

--- r-base/tests/testthat/testRPostgreSQL.R
+++ r-base/tests/testthat/testRPostgreSQL.R
@@ -1,24 +1,22 @@
 library(RPostgreSQL)
 
-print ("Test RPostgreSQL...")
+print("Test RPostgreSQL...");
 
-# create a connection
-# save the password that we can "hide" it as best as we can by collapsing it
+# create a connection save the password that we can 'hide' it as best as we can
+# by collapsing it
 pw <- {
-  "data"
+    "data"
 }
 
 # loads the PostgreSQL driver
-drv <- dbDriver("PostgreSQL")
+drv <- dbDriver("PostgreSQL");
 
-# creates a connection to the Postgres database
-# note that "con" will be used later in each connection to the database
-con <- dbConnect(drv, dbname = "data",
-                 host = "db", port = 5432,
-                 user = "data", password = pw)
-rm(pw) # removes the password
+# creates a connection to the Postgres database note that 'con' will be used
+# later in each connection to the database
+con <- dbConnect(drv, dbname = "data", host = "db", port = 5432, user = "data", password = pw);
+rm(pw);  # removes the password
 
 # check for the cartable
-dbExistsTable(con, "sample_data")
+dbExistsTable(con, "sample_data");
 
-print ("Success!")
+print("Success!");
